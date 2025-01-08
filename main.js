4ac4a6a1be442ab051a733fcb2c3bbfd556c4f34
Silvano-bule <silvanobule520@gmail.com>
2025-01-08 16:44:11 +0100

    Sempre atualizando

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const itens = document.querySelectorAll('.menu_itens nav a')
 
 itens.forEach(element => {
-    element.addEventListener('click', function(){
-       itens.forEach(el => el.classList.remove('design_class'))
-
-       this.classList.add('design_class')
+    element.addEventListener('click', function () {
+        itens.forEach(el => el.classList.remove('design_class'))
+        this.classList.add('design_class')
     })
 });
