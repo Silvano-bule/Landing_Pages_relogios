7e6a31dc8ec1f893137c0b3b73c888058c5782e0
Silvano-bule <silvanobule520@gmail.com>
2025-01-12 21:05:30 +0100

    Inovação
    Sempre mudando

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const itens = document.querySelectorAll('.menu_itens nav a')
 const menu = document.querySelector('.menu')
+const iconeMenu = document.querySelector('.icone_menu')
+const iconeClosed = document.querySelector('.icone_closed')
+const menuNavBar = document.querySelector('.menu .menu_itens nav')
 
 itens.forEach(element => {
     element.addEventListener('click', function () {
@@ -15,3 +18,25 @@ window.addEventListener('scroll', function(){
         menu.classList.remove('menu_background')
     }
 })
+
+const ShowMenu = () =>{
+    menuNavBar.classList.add('New_Nav_Bar')
+
+    window.addEventListener('scroll', function(){
+        if(window.scrollY){
+            menu.classList.remove('menu_background')
+        }
+    })
+}
+
+const removeMenu = () =>{
+    menuNavBar.classList.remove('New_Nav_Bar')
+
+    window.addEventListener('scroll', function(){
+        if(window.scrollY > 50){
+            menu.classList.add('menu_background')
+        }
+    })
+}
+iconeMenu.addEventListener('click', () => ShowMenu())
+iconeClosed.addEventListener('click', () =>  removeMenu())
